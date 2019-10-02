dfc4835f7e15ac3f8a33dc772b0a7c6aa355e649
tylerbyte666 <tjgeyser1@gmail.com>
2019-10-02 21:25:54 +0200

    Added stream for the echo server

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 const http = require('http');
 
 http.createServer((request, response) => {
-  const { headers, method, url } = request;
-  let body = [];
   request.on('error', (err) => {
     console.error(err);
-  }).on('data', (chunk) => {
-    body.push(chunk);
-  }).on('end', () => {
-    body = Buffer.concat(body).toString();
-    response.on('error', (err) => {
-      console.error(err);
-    });
-    
-    response.writeHead(200, {'Content-Type': 'application/json'})
-
-    const responseBody = { headers, method, url, body };    
-    response.write(JSON.stringify(responseBody));
-    
-    response.end(JSON.stringify(responseBody));
+    response.statusCode = 400;
+    response.end();
   });
+  response.on('error', (err) => {
+    console.error(err);
+  });
+  if (request.method === 'POST' && request.url === '/echo') {
+    request.pipe(response);
+  } else {
+    response.statusCode = 404;
+    response.end();
+  }
 }).listen(8080);
